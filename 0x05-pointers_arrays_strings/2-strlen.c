7178341e52a1c3ac60a9fe5ad2464aaa654cfd1e
offering <princessfunmilola@ymail.com>
2023-06-26 12:49:56 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,10 +9,10 @@ int _strlen(char *s)
 {
 	int length = 0;
 
-	while(*s != \0\)
-			{
-			length++;
-			s++;
-			}
-			return (length); 
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
 }
