3cd067032e301915ca519362cf24b7bd4fbf3c36
offering <princessfunmilola@ymail.com>
2023-06-26 14:19:51 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints half of a string
+ * if odd, n = (length_of _the_string -1)/2
+ * @str: input
+ * Return: half of the input
+ */
+void puts_half(char *str)
+{
+	int x, n, length;
+
+	length = 0;
+	for (x = 0; str[x] != '\0'; x++)
+		length++;
+
+	n = (length / 2);
+
+	if ((length % 2) == 1)
+		n = ((length + 1) / 2);
+
+	for (x = n; str[x] != '\0'; x++)
+		_putchar(str[x]);
+	_putchar('\n')
+}
