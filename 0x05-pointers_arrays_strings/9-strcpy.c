823eddd807a5699a0b610257c274bfa2a2650100
offering <princessfunmilola@ymail.com>
2023-06-26 15:12:16 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,11 @@ char *_strcpy(char *dest, char *src)
 	int x = 0;
 	int y = 0;
 
-	while (*(src + y)!= '\0')
+	while (*(src + x)!= '\0')
 	{
 		x++;
 	}
-	for ( ; y < 1; y++)
+	for ( ; y < x; y++)
 	{
 		dest[y] = src[y];
 	}
