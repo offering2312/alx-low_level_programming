43cc9df046b4a2390e7004dfaf4eb8f0b24cc9d6
offering <princessfunmilola@ymail.com>
2023-07-03 07:37:44 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,12 +9,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int n = 0;
+	int r = 0;
 	int i = n;
 
-	for (; n < i; n++)
+	for (; r < i; r++)
 	{
-		dest[n] = src[n];
+		dest[r] = src[r];
 		n--;
 	}
 	return (dest)
